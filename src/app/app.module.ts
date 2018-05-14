52b0c62dd95ab72d8ddd4e7d022bbaa5e4933276
Pablo Barreyro <Pabarreyro@gmail.com>
2018-05-13 18:34:06 -0700

    Add ng-bootstrap dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
@@ -25,6 +26,7 @@ import { routing } from './app.routing';
     ConnectComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
